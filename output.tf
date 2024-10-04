079fd63ba682753b335c3269c2f0d9ef46879df3
manifullstackdeveloper <55570866+manifullstackdeveloper@users.noreply.github.com>
2024-10-03 22:29:01 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,8 @@
 #Log the load balancer app url
-output "app_url" {
+output "app-alb-url" {
   value = aws_alb.ecs-fargate-deployment-alb.dns_name
+}
+
+output "app-dns-name" {
+  value = aws_route53_record.ecs-fargate-deployment.name
 }
